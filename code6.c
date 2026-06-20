e5d063ab2d963357408f6e513bf9ba1407aa2f85
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-20 09:27:57 +0530

    ques added

--- code6.c
+++ code6.c
@@ -2,6 +2,14 @@
 #include <math.h>
 int main()
 {
+    /*
+    draw pattern 
+    * * * *
+    * * * * 
+    * * * * 
+    * * * * 
+    * * * * 
+    */
     for (int i = 1; i <= 4; i++)
     {
         for (int j = 1; j <= 5; j++)
