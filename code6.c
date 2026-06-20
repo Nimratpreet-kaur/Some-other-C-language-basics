ef2d62a08d49476dcaef585cf564dcf89c05110c
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-20 09:30:07 +0530

    correction

--- code6.c
+++ code6.c
@@ -10,11 +10,11 @@ int main()
     * * * * 
     * * * * 
     */
-    for (int i = 1; i <= 4; i++)
+    for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= 5; j++)
+        for (int j = 1; j <= 4; j++)
         {
-            printf("%c", '*');
+            printf("%c ", '*');
         }
         printf("\n");
     }
