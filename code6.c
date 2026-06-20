52e5e52ce12afa04f9e76c270e20a48b5ca837fb
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-20 09:24:28 +0530

    new ques

---
+++ code6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
