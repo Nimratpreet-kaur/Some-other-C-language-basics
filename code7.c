98a271ff1dc300eabae503076ef165a54da5efa1
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-22 14:30:41 +0530

    practice code added

---
+++ code7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // to check if a number is prime or not
+    int n;
+    int is_prime;
+    is_prime = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            is_prime = 0;
+            break;
+        }
+        
+    }
+    if(is_prime)
+    {
+        printf("Number is Prime.");
+    }
+    else
+    {
+        printf("Number is not prime.");
+    }
+    return 0;
+}
